541c0cc0403e4fd367da8306d45ca9bc17248a33
Anurag Narkhede <anuragnarkhede02@gmail.com>
2026-06-30 09:28:58 +0530

    Project setup: venv, db_config, connection test

---
+++ test_connection.py
@@ -0,0 +1,12 @@
+﻿from db_config import get_connection
+
+conn = get_connection()
+print("Connected successfully!")
+
+cur = conn.cursor()
+cur.execute("SELECT COUNT(*) FROM products;")
+result = cur.fetchone()
+print(f"Number of products in database: {result[0]}")
+
+cur.close()
+conn.close()
